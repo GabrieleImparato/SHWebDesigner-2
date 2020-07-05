62bc9fcfcfc1c997d1462119ba042c1e964a3be9
Shinogab1 <Shinogab1@users.noreply.github.com>
2020-07-05 16:05:13 +0200

    Added to files

---
+++ contactform.php
@@ -0,0 +1,17 @@
+<?php
+
+if (isset($_POST['submit'])) {
+  $name = $_POST['nome'];
+  $mailFrom = $_POST['mail'];
+  $subject = $_POST['oggetto'];
+  $message = $_POST['messaggio'];
+
+  $mailTo = "cobra0571@hotmail.it";
+  $headers = "Da: " . $mailFrom;
+  $textmail = "Hai ricevuto un'e-mail da " . $name . ".\n\n" . $message;
+
+  mail($mailTo,$subject,$textmail,$headers);
+  header("Location: contact.php?mailsend");
+}
+
+   ?>
